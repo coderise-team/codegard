0d042037ba5a1a9d3db69c8ac4d2980c2124e270
Artem <artem.chebanyuk@gmail.com@email.com>
2026-05-15 23:26:01 +0300

    feat(frontend): client.js

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettierConfig from 'eslint-config-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -18,5 +19,5 @@ export default defineConfig([
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
-  prettierConfig,
+  prettierConfig
 ])

a89fa61d245d09d57e99e18bc55a3e6bcb57f572
urijtrufin <yura@gmail.com>
2026-05-17 21:34:05 +0300

    Add admin in problems (fix)

--- backend/django_app/apps/problems/admin.py
+++ backend/django_app/apps/problems/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Problem
 
-# Register your models here.
+@admin.register(Problem)
+class ProblemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'difficulty', 'time_limit', 'memory_limit', 'created_at')
+    list_filter = ('difficulty',)
+    search_fields = ('title', 'description')
+

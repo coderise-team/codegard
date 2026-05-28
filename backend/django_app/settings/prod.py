45a33d99153b939df0e613c47de569d376b36c16
urijtrufin <yura@gmail.com>
2026-05-28 21:03:36 +0300

    fix: move databasus to dev, add celery services and fix prod settings

--- backend/django_app/settings/prod.py
+++ backend/django_app/settings/prod.py
@@ -9,6 +9,20 @@ ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 DATABASES = {"default": env.db("DATABASE_URL")}
 
+_redis_url = f"redis://:{env('REDIS_PASSWORD')}@redis:6379/0"
+
+CELERY_BROKER_URL = _redis_url
+CELERY_RESULT_BACKEND = _redis_url
+REDIS_URL = _redis_url
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [_redis_url],
+        },
+    },
+}
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 SECURE_HSTS_SECONDS = 31536000
